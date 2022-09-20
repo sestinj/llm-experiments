0801d8c8ade208904c5d74328a80531a714ef7cd
sestinj <33237525+sestinj@users.noreply.github.com>
2022-09-19 23:58:56 -0400

    Messing around with GPT-3

--- llm/main.py
+++ llm/main.py
@@ -1,37 +1,48 @@
-import transformers
-from transformers import BloomForCausalLM
-from transformers import BloomTokenizerFast
+import os
+import time
+from functools import lru_cache
+
+import openai
 import torch
+import transformers
+from dotenv import load_dotenv
+from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from transformers import BloomForCausalLM, BloomTokenizerFast
 
-bloom_models = ["", "-6b3", "-2b5", "-1b3", "-760m", "-360m"]
-bloom_model = "bigscience/bloom" + bloom_models[4]
+from utils.config import Settings
 
-model = BloomForCausalLM.from_pretrained(bloom_model)
-tokenizer = BloomTokenizerFast.from_pretrained(bloom_model)
+load_dotenv()
 
-def greedy(prompt: str, result_length: int=50):
-    inputs = tokenizer(prompt, return_tensors="pt")
-    return tokenizer.decode(model.generate(inputs["input_ids"], 
-                    max_new_tokens=result_length
-                    )[0])
+# bloom_models = ["", "-6b3", "-2b5", "-1b3", "-760m", "-350m"]
+# bloom_model = "bigscience/bloom" + bloom_models[5]
 
-def beam(prompt: str, result_length: int=50):
-    inputs = tokenizer(prompt, return_tensors="pt")
-    return tokenizer.decode(model.generate(inputs["input_ids"],
-                    max_new_tokens=result_length, 
-                    num_beams=2, 
-                    no_repeat_ngram_size=2,
-                    early_stopping=True
-                    )[0])
+# model = BloomForCausalLM.from_pretrained(bloom_model)
+# tokenizer = BloomTokenizerFast.from_pretrained(bloom_model)
 
-def sample(prompt: str, result_length: int=50):
-    inputs = tokenizer(prompt, return_tensors="pt")
-    return tokenizer.decode(model.generate(inputs["input_ids"],
-                    max_new_tokens=result_length, 
-                    do_sample=True, 
-                    top_k=50, 
-                    top_p=0.9
-                    )[0])
+# def greedy(prompt: str, result_length: int=50):
+#     inputs = tokenizer(prompt, return_tensors="pt")
+#     return tokenizer.decode(model.generate(inputs["input_ids"], 
+#                     max_new_tokens=result_length
+#                     )[0])
+
+# def beam(prompt: str, result_length: int=50):
+#     inputs = tokenizer(prompt, return_tensors="pt")
+#     return tokenizer.decode(model.generate(inputs["input_ids"],
+#                     max_new_tokens=result_length, 
+#                     num_beams=2, 
+#                     no_repeat_ngram_size=2,
+#                     early_stopping=True
+#                     )[0])
+
+# def sample(prompt: str, result_length: int=50):
+#     inputs = tokenizer(prompt, return_tensors="pt")
+#     return tokenizer.decode(model.generate(inputs["input_ids"],
+#                     max_new_tokens=result_length, 
+#                     do_sample=True, 
+#                     top_k=50, 
+#                     top_p=0.9
+#                     )[0])
 
 def first_sentence(full: str):
     return full.split(full, ".")[0] + "."
@@ -40,30 +51,92 @@ def first_line(full: str):
     return full.splitlines()[0]
 
 def first_line_after(full: str, prefix: str):
-    print(full.replace(prefix, "").splitlines())
     lines = full.replace(prefix, "").splitlines()
     if lines[0] == "" and len(lines) > 0:
         return lines[1]
     else:
         return lines[0]
 
-prompt_schema = """
-openapi: 3.1.0
-x-stoplight:
-  id: yjuh6ia0uicil
-info:
-  title: test
-  version: '1.0'
-servers:
-  - url: 'http://localhost:3000'
-paths:
-"""
-curr = "The following is an OpenAPI schema describing a petstore:\n" + prompt_schema
-for i in range(10):
-    print(curr)
-    resp = "" # input("Continue?(y/n) ")
-    if resp == 'n':
-        break
+def first_sentence_after(full: str, prefix: str):
+    sentences = full.replace(prefix, "").split(".")
+    if sentences[0] == "" and len(sentences) > 0:
+        return sentences[1]
     else:
-        curr += first_line_after(greedy(curr, result_length=25), curr) + "\n"
-print(curr)
+        return sentences[0]
+
+# prompt_schema = """
+# openapi: 3.1.0
+# x-stoplight:
+#   id: yjuh6ia0uicil
+# info:
+#   title: test
+#   version: '1.0'
+# servers:
+#   - url: 'http://localhost:3000'
+# paths:
+# """
+# curr = "The following is an OpenAPI schema describing a petstore:\n" + prompt_schema
+# for i in range(10):
+#     print(curr)
+#     resp = "" # input("Continue?(y/n) ")
+#     if resp == 'n':
+#         break
+#     else:
+#         curr += first_line_after(greedy(curr, result_length=25), curr) + "\n"
+# print(curr)
+
+
+
+# FAST API #
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@lru_cache
+def get_settings():
+    settings = Settings()
+    # print(settings.dict())
+    # openai.api_key = settings.openai_api_key
+    openai.api_key = os.environ["OPENAI_API_KEY"]
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/bloom/")
+async def generate(text: str):
+    return {"generated": first_sentence_after(greedy(text, result_length=25), text)};
+
+@app.get("/openai/")
+async def generate(text: str, settings: Settings = Depends(get_settings)):
+            return openai.Completion.create(
+                model="text-davinci-002",
+                prompt=text,
+                max_tokens=25,
+                temperature=0.6,
+                echo=False
+            ).choices[0].text
+
+@app.get("/openai/compare/")
+async def generate(text:str , settings: Settings = Depends(get_settings)):
+    names = ["ada-001", "babbage-001", "curie-001", "davinci-001", "davinci-002"]
+    resp = {};
+    for name in names:
+        ti = time.time()
+        completion = openai.Completion.create(
+            model="text-ada-001",
+            prompt=text,
+            max_tokens=25,
+            temperature=0.6,
+        ).choices[0].text
+        tf = time.time()
+        resp[name] = {"time": tf - ti, "text": completion}
+    return resp
--- ui-experiments/src/routes/textarea++/+page.svelte
+++ ui-experiments/src/routes/textarea++/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-    import {taskQueue, logs} from "../../store";
+
     import TaskCard from "../../lib/TaskCard.svelte";
-    import {taskData} from "../../taskData";
-    import type {Task} from "../../taskData";
+    import { logs,taskQueue } from "../../store";
+    import type { Task } from "../../taskData";
+    import { taskData } from "../../taskData";
 
     /** Thoughts
      * What if you could include a delimeter that indicated "exact" phrases, like commands? For example, if you typed "[search]", then all of the text around it would get scooped up as context and processed by some predetermined search action. Might also want explicit parameters like "[search](how to tie a knot)".
@@ -11,8 +12,27 @@
      * Show text from relevant links, Tweets, posts, etc... from around the internet or from your personal Notion, Obsidian, Google Docs, computer, etc... and easily save these to a list. Great for writing blog posts.
      * Make suggestions about mood, or better vocabulary (basically Grammarly)
      * Translate stuff in other languages
+     * 
+     * WHEN I'M WRITING SOMETHING:
+     * I want to give a summary of a paragraph and have it written for me
+     * I want to request variations of the current sentence
+     * I want to be able to create my own custom prompts and apply them with shortcut keys. They should be applied to either the full text, or just the current sentence, or some other settings.
+     * I want to know which sentences were generated, and I should be able to click on them and retry the prompt to get something else.
     */
 
+    /**
+     * Favorite prompts:
+     * Provide evidence for the above claim:
+     * Write a paragraph for the above topic sentence:
+     * Provide evidence to back up the above claim:
+     * Write the next sentence in the above story:
+     */
+
+    let textarea: HTMLTextAreaElement;
+    let shortcutModalOpen = false;
+    let shortcutInput: HTMLInputElement;
+    let eventListenerToRemove: any;
+    let shortcutInputValue: KeyboardEvent | null = null;
     let taskSet = new Set<Task>();
 
     function addLog(log: string) {
@@ -47,6 +67,7 @@
     }
 
     function handleEvent(event: InputEvent) {
+        // console.log(event);
         switch (event.inputType) { 
             case "insertText":
                 // console.log(event.data);
@@ -63,13 +84,80 @@
         })
     }
 
+    interface KeyShortcut {
+        keymask: number, // Bitmask of the modifier keys, in order alt, ctrl, shift, meta (from right to left)
+        callback: (event: KeyboardEvent, fullText: string) => Promise<string> | string,
+        output: "completion" | "log" | "task" | string;
+    }
+    function shortcutToString(key: string, shortcut: KeyboardEvent) {
+        let modifiers = "";
+        if (shortcut.altKey) modifiers += "⎇";
+        if (shortcut.ctrlKey) modifiers += "^";
+        if (shortcut.shiftKey) modifiers += "⇧";
+        if (shortcut.metaKey) modifiers += "⌘";
+        return `${modifiers}${key.toUpperCase()}`;        
+    }
+    interface KeyShortcuts {
+        [key: string]: KeyShortcut[]
+    }
+    let shortcuts: KeyShortcuts = {
+        "k": [{
+            keymask: 0b1000,
+            callback: async (event: KeyboardEvent, fullText: string) => {
+                return await requestCompletion(fullText);
+            },
+            output: "completion"
+        }]
+    };
+
+    const calcKeyMask = (event: KeyboardEvent) => {
+        let mask = event.altKey ? 0b0001 : 0;
+        mask |= event.ctrlKey ? 0b0010 : 0;
+        mask |= event.shiftKey ? 0b0100 : 0;
+        mask |= event.metaKey ? 0b1000 : 0;
+        return mask;
+    }
+
+    const keydownHandler = (event: KeyboardEvent) => {
+            if (shortcuts[event.key]) {
+                let mask = calcKeyMask(event);
+                shortcuts[event.key].forEach(async (shortcut) => {
+                    if (shortcut.keymask === mask) {
+                        let text = await shortcut.callback(event, textarea.value);
+                        switch (shortcut.output) {
+                            case "completion":
+                                textarea.value += text;
+                                break;
+                            case "log":
+                                addLog(text);
+                                break;
+                            case "task":
+                                break;
+                        }
+                        
+                    }
+                })
+            }
+        };
+
+    // To handle keyboard shortcuts
+    if (typeof document !== "undefined") {
+        document.addEventListener("keydown", keydownHandler);
+    }
+    
+    async function requestCompletion(text: string, max_tokens: number = 25): Promise<string> {
+        let resp = await fetch("http://localhost:8000/openai?text=" + encodeURIComponent(text) + "&max_tokens=" + max_tokens);
+        let completion = await resp.text();
+        return completion.substring(1, completion.length - 1);
+    }
+
 </script>
 
 
 <h1>{"<textarea++ />"}</h1>
 
 <div id="magic-div-outer">
-    <textarea rows="20" class="magic-textarea" on:input={handleEvent} tabindex="0"></textarea>
+    <textarea rows="20" class="magic-textarea" on:input={handleEvent} tabindex="0" bind:this={textarea}></textarea>
     <div id="magic-div-inner">
         {#each $taskQueue as task, idx}
             <TaskCard {task} addLog={addLog} tabindex={idx + 1}></TaskCard>
@@ -82,6 +170,74 @@
         <div>{log}</div>
     {/each}
 </pre>
+
+<button style:margin="auto" on:click={() => {
+    shortcutModalOpen = true;
+}}>Add Shortcut</button>
+<br><br>
+
+<div id="shortcut-div" hidden={!shortcutModalOpen}>
+    <form on:submit|preventDefault={(event) => {{
+        let data = new FormData(event.target);
+        let tokens = data.get("tokens");
+        let output = data.get("output");
+        if (!shortcutInputValue || !tokens || !output) return;
+
+        let max_tokens = parseInt(tokens.toString());
+        let shortcut = {
+            keymask: calcKeyMask(shortcutInputValue),
+            callback: async (event, fullText) => {
+                return await requestCompletion(`${fullText}\n${data.get("prompt")}`, max_tokens);
+            },
+            output: output.toString()
+        }
+        if (typeof shortcuts[shortcutInputValue.key] === "undefined") {
+            shortcuts[shortcutInputValue.key] = [shortcut];
+        } else {
+            shortcuts[shortcutInputValue.key].push(shortcut);
+        }
+        document.removeEventListener("keydown", eventListenerToRemove);
+        addLog(`New shortcut "${data.get("title")}": ${shortcutToString(shortcutInputValue.key, shortcutInputValue)}`);
+        shortcutModalOpen = false;
+    }}}>
+        <label for="title">Title</label>
+        <input id="title" name="title" type="text" />
+        <br><br>
+        
+        <label for="prompt">Prompt</label>
+        <input id="prompt" name="prompt" type="text" />
+        <br><br>
+
+        <label for="tokens">Tokens</label>
+        <input id="tokens" name="tokens" type="number" />
+        <br><br>
+
+        <label for="output">Output</label>
+        <select id="output" name="output">
+            <option value="log">Log</option>
+            <option value="completion">Completion</option>
+            <option value="task">Task</option>
+        </select>
+
+        <label for="shortcut">Shortcut</label>
+        <input id="shortcut" name="shortcut" type="text" bind:this={shortcutInput} on:focus={() => {
+            eventListenerToRemove = (event) => {
+                if (event.key === "Escape" || event.key === "Tab" || event.key === "Enter") {
+                    return;
+                }
+                if (!(event.altKey || event.metaKey || event.ctrlKey || event.shiftKey)) {
+                    return;
+                }
+                shortcutInputValue = event;
+                shortcutInput.value = shortcutToString(event.key, event);
+            };
+            document.addEventListener("keydown", eventListenerToRemove);
+        }}
+        />
+        <br><br>
+        <input type="submit" value="Done">
+    </form>
+</div>
 
 <style>
     .magic-textarea {
@@ -114,5 +270,9 @@
     .log-output {
         background-color: #fff6;
         border-radius: 12px;
+    }
+
+    #shortcut-div form {
+        text-align: center;
     }
 </style>
